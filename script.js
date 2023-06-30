78b3e48bacc13e50a3eb83b5255d7f48cfdbe4c6
mikhaelapg <mikhaelapg00@gmail.com>
2023-06-30 20:18:17 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict'; // Didalam mode strict, tidak memperbolehkan anda menggunakan variabel yang belum dideklarasikan sebelumnya.
+
+let hadDriversLicense = false;
+let passTest = true;
+
+if (passTest) hadDriversLicense = true;
+if (hadDriversLicense) console.log('I can drive :D');
+
+// const interface = 'Audio';
+// const private = 534;
+// const if = 23;
