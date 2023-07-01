99aa5af7a9e1b2f17867e62fb52e9a4161cd4950
mikhaelapg <mikhaelapg00@gmail.com>
2023-07-01 18:40:06 +0700

    function declaration and expression in js

--- script.js
+++ script.js
@@ -33,3 +33,23 @@ const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
 const num = Number('23');
+
+//function declaration
+const age1 = calcAge1(1991);
+
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+
+// const age1 = calcAge1(1991);
+console.log(age1);
+
+// function expression
+// const age2 = calcAge2(1991);
+
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
