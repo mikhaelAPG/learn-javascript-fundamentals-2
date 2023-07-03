b5d82b1e00c94e2a3959d9a602a0fad073ef2505
mikhaelapg <mikhaelapg00@gmail.com>
2023-07-03 11:42:06 +0700

    add coding challenge 2 for javascript fundamental part 2

--- script.js
+++ script.js
@@ -198,3 +198,14 @@ console.log(friends.includes(23));
 if (friends.includes('Steven')) {
     console.log('You have a friend called Steven');
 }
+
+// coding challenge 2
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2; 
+}
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0], calcTip(bills[1], calcTip(bills[2])))];
+// bonus
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(bills, tips, total);
