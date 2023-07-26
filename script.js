43692ebc83460811e02f8d890c3fc03533d4518c
mikhaelAPG <mikhaelapg00@gmail.com>
2023-07-26 22:30:39 +0700

    the for loop in js

--- script.js
+++ script.js
@@ -326,3 +326,19 @@ if (mark.bmi > john.bmi) {
 } else if (john.bmi > mark.bmi) {
     console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`);
 }
+
+// console.log('Lifting weights repetition 1');
+// console.log('Lifting weights repetition 2');
+// console.log('Lifting weights repetition 3');
+// console.log('Lifting weights repetition 4');
+// console.log('Lifting weights repetition 5');
+// console.log('Lifting weights repetition 6');
+// console.log('Lifting weights repetition 7');
+// console.log('Lifting weights repetition 8');
+// console.log('Lifting weights repetition 9');
+// console.log('Lifting weights repetition 10');
+
+// for loop keeps running while condition is TRUE
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights repetition ${rep}`);
+}
