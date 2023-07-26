5bb5fdbfb01ceaa3e32ce5a17a2a07eb847bfb85
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2023-07-26 22:22:17 +0700

    conding challenge 7

--- script.js
+++ script.js
@@ -293,3 +293,36 @@ console.log(jonas.age);
 // challenge
 // "Jonas is a 46-year old teacher, and he has a driver's license"
 console.log(jonas.getSummary());
+
+const mark = {
+    fullName: 'Mark Miller',
+    mass: 78,
+    height: 1.69,
+    calcBMI: function() {
+        this.bmi = this.mass / this.height ** 2;
+        return this.bmi;
+    }
+};
+
+const john = {
+    fullName: 'John Smith',
+    mass: 92,
+    height: 1.95,
+    calcBMI: function() {
+        this.bmi = this.mass / this.height ** 2;
+        return this.bmi;
+    }
+};
+
+mark.calcBMI();
+john.calcBMI();
+
+console.log(mark.bmi, john.bmi);
+
+// "John Smith's BMI (28.3) is higher than mark Miller's (23.9)!"
+
+if (mark.bmi > john.bmi) {
+    console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`);
+} else if (john.bmi > mark.bmi) {
+    console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`);
+}
